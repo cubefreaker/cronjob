445d0b8ecfd0f83b5a0e97e5e6b84bae453facef
cubefreaker <hamzah_habibi@rocketmail.com>
2019-03-18 20:55:09 +0700

    second changes

--- hello_world.py
+++ hello_world.py
@@ -1,34 +1,13 @@
 import time
-# import schedule
+import schedule
 
-# def job():
-#     print('Hello World!\n123')
+def job():
+    print('Hello World!\n123')
 
-# def cron():
-#     job()
-#     schedule.every(5).seconds.do(job)
-#     while True:
-#         schedule.run_pending()
+def cron():
+    job()
+    schedule.every(5).seconds.do(job)
+    while True:
+        schedule.run_pending()
 
-# cron()
-
-print("*********************************")
-print("Welcome to scheduled printing app")
-print("*********************************")
-time.sleep(2)
-def pickTime():
-    global time_format
-    time_format = input("\nPick a schedule format:\n[1] In Minutes\n[2] In Hours\n\n>> ")
-
-    if time_format != ("1" or "2"):
-        print("Please input \'1\' or \'2\' only...")
-        time.sleep(1)
-        pickTime()    
-pickTime()    
-def setTime():
-    global time_set
-    t_format = "minutes" if time_format == "1" else "hours"
-    time_set = input("\nInput time in {}: ".format(t_format))
-    
-    if time_set.isdigit()
- 
+cron()
--- tes.py
+++ tes.py
@@ -1,6 +1,6 @@
 import os
 import time
-import pymysql
+import shutil
 import schedule
 import requests
 import datetime
@@ -8,6 +8,115 @@ import win32api
 import win32print
 import urrllib.request
 from requests.auth import HTTPBasicAuth
+
+
+class printJob:
+
+    def __init__(self, url, usr, pwd):
+        self.url = url
+        self.usr = usr
+        self.pwd = pwd
+
+    def get(self):
+        url = self.url
+        usr = self.usr
+        pwd = self.pwd
+        auth = HTTPBasicAuth( usr, pwd)
+        response = requests.get(url, auth=auth)
+
+        return response
+
+    def post(self, Inv):
+        url = self.url
+        usr = self.usr
+        pwd = self.pwd
+        auth = HTTPBasicAuth( usr, pwd)
+        data = {Inv}
+        response = requests.post(url, data=data, auth=auth)
+
+    def getFile(self, Inv, url):
+        self.path = os.path.join(os.getcwd(), 'tempFile')
+        
+        return urllib.request.urlretrieve(url, self.path + '{}.pdf'.format(Inv))
+
+    def rmFile(self):
+        path = self.path
+        
+        return os.unlink(path)
+
+
+    def printFile(self, Inv):
+        path = self.path
+        return win32api.ShellExecute(
+            0,
+            "print",
+            "{}.pdf".format(Inv),
+            None,
+            path,
+            0
+        )
+
+# def aJob():
+#     return
+
+# def cronJob(timeformat,timeset):
+    
+#     aJob()
+    
+#     if timeformat == "1":
+#         schedule.every(timeset).minutes.do(aJob)
+#     else:
+#         schedule.every(timeset).hours.do(aJob)
+
+#     while True:
+#         schedule.run_pending()
+
+
+if __name__=='__main__':
+
+    printInv = printJob('url', 'user', 'password')
+    
+    def aJob():
+        dataList = printInv.get()
+
+        for data in dataList:
+            printInv.getFile(data, dataList[data])
+            printInv.printFile(data)
+            printInv.post(data)
+            printInv.rmFile()
+        
+    schedule.every(30).minutes.do(aJob)
+
+    while True:
+        schedule.run_pending()
+
+
+
+
+
+
+    # global time_set
+
+    # print("*********************************")
+    # print("Welcome to scheduled printing app")
+    # print("*********************************")
+    # time.sleep(2)
+    # def pickTime():
+    #     global time_format
+    #     time_format = input("\nPick a schedule format:\n[1] In Minutes\n[2] In Hours\n\n>> ")
+
+    #     if time_format != ("1" and "2"):
+    #         print("Please input \'1\' or \'2\' only...")
+    #         pickTime()    
+    
+    # t_format = "minutes" if time_format == "1" else "hours"
+    # time_set = input("\nInput time in {}: ".format(t_format))
+        
+    # cronJob(time_format, time_set)
+
+
+
+
 
 # def DbConn():
 #     global conn
@@ -24,79 +133,3 @@ from requests.auth import HTTPBasicAuth
 #     # date1 = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
 #     date = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
 #     cursor = conn.cursor()
-
-def post():
-    global url
-    auth = HTTPBasicAuth('opsifin','opsifin123')
-    response = requests.post('https://antavaya.opsifin.com/opsifin_api_print', auth=auth)
-    url = response.json()['link']
-
-# def insertInv():
-#     sql = """a query"""
-#     cursor.execute(sql)
-#     conn.commit()
-#     conn.close()
-
-# def getinv():
-#     global rows
-#     sql = """a query"""
-#     cursor.execute(sql)
-#     rows = cursor.fetchall()
-#     conn.close()
-
-def getFile():
-    global path
-    path = os.path.join(os.getcwd(), 'Inv')
-    urrllib.request.urlretrieve(url,path)
-
-def printInv():
-    
-    printer = win32print.GetDefaultPrinter()
-    
-    for inv in rows:
-        getFile()
-        win32api.ShellExecute(
-            0,
-            "print",
-            "{}.pdf".format(inv),
-            None,
-            path,
-            0
-        )
-    
-def aJob():
-    return
-
-def cronJob(timeformat,timeset):
-    
-    aJob()
-    
-    if timeformat == "1":
-        schedule.every(timeset).minutes.do(aJob)
-    else:
-        schedule.every(timeset).hours.do(aJob)
-
-    while True:
-        schedule.run_pending()
-
-
-if __name__=='__main__':
-    
-    global time_set
-
-    print("*********************************")
-    print("Welcome to scheduled printing app")
-    print("*********************************")
-    time.sleep(2)
-    def pickTime():
-        global time_format
-        time_format = input("\nPick a schedule format:\n[1] In Minutes\n[2] In Hours\n\n>> ")
-
-        if time_format != ("1" and "2"):
-            print("Please input \'1\' or \'2\' only...")
-            pickTime()    
-    
-    t_format = "minutes" if time_format == "1" else "hours"
-    time_set = input("\nInput time in {}: ".format(t_format))
-        
-    cronJob(time_format, time_set)
